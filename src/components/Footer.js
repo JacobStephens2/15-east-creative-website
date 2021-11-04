80c742b155de66aa5e5242a1231e00a851cef0a6
Jacob Stephens <jacob@stewardgoods.com>
2021-11-04 12:35:53 -0400

    restyle footer, adding phone and email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,30 +26,37 @@ const Footer = (props) => {
   return (
     <div className="footer">
       <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="footer-inner">
-              <Link to="/">Home</Link>
+        <Row>
+          <Col md={1}></Col>
+          <Col>
+            <div className="row">
+              <div className="col-12">
+                <div className="footer-inner">
+                  <Link to="/">Home</Link>
 
-              <ul>
-                {data.allFooterMenuJson.edges.map(({ node }) => (
-                  <li key={node.name}>
-                    <Link to={node.url}>{node.name}</Link>
-                  </li>
-                ))}
-                <li>
-                  <Button target="/contact" buttonText="CONTACT" />
-                </li>
-              </ul>
+                  <ul>
+                    {data.allFooterMenuJson.edges.map(({ node }) => (
+                      <li key={node.name}>
+                        <Link to={node.url}>{node.name}</Link>
+                      </li>
+                    ))}
+                    <li>
+                      <Button target="/contact" buttonText="CONTACT" />
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <Row className="footer-contact-info">
-          <hr />
-          <a href="tel:16107802023">
-            <p>610.780.2023</p>
-          </a>
-          <a href="mailto:team@15eastcreative.com">team@15eastcreative.com</a>
+            <Row className="footer-contact-info">
+              <hr />
+              <a href="tel:16107802023">
+                <p>610.780.2023</p>
+              </a>
+              <a href="mailto:team@15eastcreative.com">
+                team@15eastcreative.com
+              </a>
+            </Row>
+          </Col>
         </Row>
       </div>
     </div>
