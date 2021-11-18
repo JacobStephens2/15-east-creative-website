2f9631ce5e286c1f80addedb01df91e96be4ee8a
Jacob Stephens <jacob@stewardgoods.com>
2021-11-18 12:06:29 -0500

    get Adobe fonts working on Safari

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
-import favicon from '../../static/favicon-32x32.svg';
+import React from "react";
+import Helmet from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
+import favicon from "../../static/favicon-32x32.svg";
 
-const SEO = props => (
+const SEO = (props) => (
   <StaticQuery
     query={detailsQuery}
-    render={data => {
+    render={(data) => {
       const title = props.title || data.site.siteMetadata.title;
       return (
         <Helmet
           htmlAttributes={{
-            lang: 'en'
+            lang: "en",
           }}
           title={title}
-          titleTemplate={props.title ? `%s` : `%s - ${data.site.siteMetadata.title}`}
+          titleTemplate={
+            props.title ? `%s` : `%s - ${data.site.siteMetadata.title}`
+          }
           link={[
-            { rel: 'shortcut icon', type: 'image/svg', href: `${favicon}` }
+            { rel: "shortcut icon", type: "image/svg", href: `${favicon}` },
           ]}
-        />
+        >
+          <link
+            rel="stylesheet"
+            href="https://use.typekit.net/oxs7yjf.css"
+          ></link>
+        </Helmet>
       );
     }}
   />
 );
-
-SEO.defaultProps = {
-  lang: 'en',
-  meta: [],
-  keywords: []
-};
 
 export default SEO;
 
